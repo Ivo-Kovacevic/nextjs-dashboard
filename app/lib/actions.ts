62b3941aedecb9310ae136b520041777c998f50b
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2025-01-12 19:34:26 +0100

    fix eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -108,7 +108,6 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
-    console.error(error);
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -22,9 +22,12 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  async function deleteInvoiceWithFormData(formData: FormData) {
+    await deleteInvoice(id); // Call the original function
+  }
+  
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={deleteInvoiceWithFormData}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
